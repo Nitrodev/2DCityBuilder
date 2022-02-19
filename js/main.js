06ecb482d265f0b9c8b0688f6edbd065f67d2913
Nitrodev <jukka.keranen@hotmail.fi>
2022-02-19 19:55:38 +0200

    Change to spritesheets

--- js/main.js
+++ js/main.js
@@ -3,14 +3,13 @@ const mousePos = document.querySelector("#pos");
 const ctx = canvas.getContext("2d");
 const cRect = canvas.getBoundingClientRect();
 
-let selected = false; // FIX THIS LATER
-let selectedImg = new Image(32, 32);
-let builtImg = new Image(32, 32);
+let selected = 'select'; // FIX THIS LATER
+let builtImg;
 
 let map = create2DArray(10);
 
 function setup() {
-  selectedImg.src = 'img/select.png';
+  images.loadSpriteSheet('./sprites.json');
   for(let x = 0; x < 10; x++) {
     for(let y = 0; y < 10; y++) {
       map[x][y] = new Tile(x, y);
@@ -27,8 +26,8 @@ function draw() {
         ctx.fillStyle = map[x][y].terrain.color;
         ctx.fillRect(x*32, y*32, 32, 32);
       } else {
-        builtImg.src = map[x][y].structure.img;
-        ctx.drawImage(builtImg, x*32, y*32);
+        let built = map[x][y].structure.img;
+        images.draw(ctx, built, x*32, y*32);
       }
     }
   }
@@ -37,8 +36,7 @@ function draw() {
 function startBuilding(structure) {
   switch (structure) {
     case 'road':
-      selected = true;
-      selectedImg.src = 'img/roadEW.png'
+      selected = structure+'EW';
       break;
   }
 }
@@ -48,17 +46,17 @@ canvas.addEventListener('mousemove', (e) => {
   let posX = Math.floor(getMousePos(cRect, e).x/32);
   let posY = Math.floor(getMousePos(cRect, e).y/32);
   draw();
-  ctx.drawImage(selectedImg, posX*32, posY*32);
-  mousePos.innerHTML = `X: ${posX} | Y: ${posY}`;
+  images.draw(ctx, selected, posX*32, posY*32);
+  mousePos.innerHTML = `X: ${posX} | Y: ${posY} \n Selected: ${selected || 'Nothing'}`;
 });
 
 canvas.addEventListener('click', (e) => {
   let posX = Math.floor(getMousePos(cRect, e).x/32);
   let posY = Math.floor(getMousePos(cRect, e).y/32);
-  if(selected == false) {
+  if(selected == 'select') {
     return;
   } else {
-    let built = new Structure(selectedImg.src);
+    let built = new Structure(selected);
     map[posX][posY].structure = built;
     draw();
   }
@@ -68,8 +66,7 @@ canvas.addEventListener('click', (e) => {
 // Clear selected image
 document.addEventListener('keydown', (e) => {
   if(e.code == 'Escape') {
-    selected = false;
-    selectedImg.src = 'img/select.png';
+    selected = 'select';
   }
 });
 
